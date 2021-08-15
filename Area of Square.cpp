7cdf9ef68e43d178484db56507ff79f6c14881fb
Shivam Kumar <73744517+k21shivam@users.noreply.github.com>
2021-08-15 16:47:30 +0530

    Add files via upload

---
+++ Area of Square.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,area;
+    cout << "Enter the side of square" ;
+    cin >> a;
+    area = a*a;
+    cout << "Area of Square = " << area << endl;
+    return 0;
+}
