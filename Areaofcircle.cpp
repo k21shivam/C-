8b234c6ce9e7167f439f38c79edc14950febe076
Shivam Kumar <73744517+k21shivam@users.noreply.github.com>
2021-08-19 11:28:43 +0530

    Add files via upload

---
+++ Areaofcircle.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define pi 3.14
+int main()
+{
+int r,area;
+cout << "Enter the radius of circle - ";
+cin >> r;
+area = pi*r*r;
+cout << "Area of the circle is " << area;
+return 0;
+}
---
+++ CircumferenceofCircle.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define pi 3.14
+int main()
+{
+ int r,cre;
+ cout << "Enter the radius - ";
+ cin >> r;
+ cre = 2*pi*r;
+ cout << "Circumference is " << cre;
+ return 0;
+}
