c417d7ddaf51b8308e548dc99fc85785ab2757ca
Shivam Kumar <73744517+k21shivam@users.noreply.github.com>
2021-08-19 10:25:18 +0530

    Add files via upload

---
+++ AreaofRectangle.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,area;
+    cout << "Enter the length and breadth of Reactangle - ";
+    cin >> a >> b;
+    area = a*b;
+    cout << "Area of Rectangle is " << area << endl;
+    return 0;
+}
