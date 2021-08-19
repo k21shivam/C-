d15c95d63f90ac01238a6d211820d82e2a4b7e39
Shivam Kumar <73744517+k21shivam@users.noreply.github.com>
2021-08-19 15:11:09 +0530

    Add files via upload

---
+++ CelsiustoFahrenheit.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float c,f;
+    cout << "Enter the temp. in celcius - ";
+    cin >> c;
+    f = (9*c+160)/5;
+    cout << "Temp. in fahrenhite is - " << f;
+    return 0;
+
+}
---
+++ FahrenhiettoCelsius.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float c,f;
+    cout << "Enter the temp. in fahrenhite - ";
+    cin >> f;
+    c = (5*f-160)/9;
+    cout << "Temp. in celcius is - " << c;
+    return 0;
+
+}
